be9d8a3ace172080122b5ca85d53ee7be9672a89
naufal akbar <88865915+rahardjorahardjo@users.noreply.github.com>
2023-05-25 00:53:17 +0700

    login register DONE

--- rpl/login.php
+++ rpl/login.php
@@ -38,42 +38,6 @@ if(isset($_POST['signin'])){
 
 ?>
 
-
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=
-    , initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/login.css">
-    <title>Login</title>
-</head>
-<body>
-    <div class="container">
-    <main class="form-signin">
-  <form method="post">
-
-    <div class="form-floating">
-      <input type="text" class="form-control" name="username" id="floatingInput" placeholder="mirzahm">
-      <label for="floatingInput">Username</label>
-    </div>
-    <div class="form-floating">
-      <input type="password" class="form-control" name="password" id="floatingPassword" placeholder="Password">
-      <label for="floatingPassword">Password</label>
-    </div>
-
-    <div class="checkbox mb-3">
-    </div>
-    <button class="w-100 btn btn-lg" id="signin" type="submit" name="signin">Sign in</button>
-  </form>
-  <a href="register.php">dont have any account</a>
-</main>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-</body>
-</html> -->
 <!DOCTYPE html>
 <html lang="en">
   <head>
